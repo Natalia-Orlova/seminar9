485b39406831ff9a9efef481cb0e773998c73436
NataliaOrlova <natakarelskaya@yandex.ru>
2022-09-02 11:50:20 +0400

    рекурсия

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите число M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNum(num1, num2);
+void PrintNaturalNum(int m1, int n1)
+{
+    Console.Write($"{m1}, ");
+    if (m1 > n1)
+    PrintNaturalNum(m1-1,n1);
+
+    else if (m1 < n1)
+    PrintNaturalNum(m1+1,n1);
+}
+
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers (int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+NaturalNumbers(number);
+
+
+// int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+// {
+//     int[,] matrix = new int[row, col];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("[");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},");
+//             else Console.Write($"{matrix[i, j],3} ");
+//         }
+//         Console.WriteLine("]");
+//     }
+// }
+
+// int[,] array2D = CreateMatrixRndInt(5, 3, 0, 9);
+// PrintMatrix(array2D);
+// Console.WriteLine();
